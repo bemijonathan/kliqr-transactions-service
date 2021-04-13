c952b071dd1aeb1a56488027a335879a13ad2e59
bemijonathan <bemijonathan@gmail.com>
2021-04-13 16:38:06 +0100

    added port to env

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const dotEnv = require('dotenv');
 dotEnv.config();
 
 const app = require('./server');
+const { InitConnection } = require('./src/db');
 const port = process.env.PORT || 4000
 
-app.listen(port , async () => {
+app.listen(port, async () => {
     await InitConnection()
-    console.log();
+    console.log('app started on POrt ', PORT);
 })
